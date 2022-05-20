59107ee113b3de620c2097419a1c08afc4db3d0b
Omerta89 <100130467+Omerta89@users.noreply.github.com>
2022-05-20 12:37:30 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Testapp from './Testapp';
+
+const y = 'HELLO'
 
 function App() {
+  var [Counter, setCounter] = useState(2);
+const [show, setshow] = useState(true)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Counter}
+      <button style={{ color: 'blue' }} >Click</button>
+      <button style={{ color: 'red' }} >Click</button>
+      <button style={{ color: 'green' }} >{y}</button>
+      <button style={{ width: Counter * 50 }}>Hello</button>
+      <button className='comp' onClick={() => { setCounter(Counter + 1) }}>{Counter}</button><br></br>
+      <button style={{ padding: Counter }} onClick={() => { setCounter(Counter++); console.log(Counter) }} >increase size</button><br></br>
+
+      <button onClick={()=>{setshow(!show)}} >{show && 'x'}</button><br></br>
+      <button onClick={()=>{setshow(!show)}} >{show ? 'x' : 'o'}</button>
+
+
+
+      <button onClick={() => { Testapp(); console.log('testapp'); }} >testapp component</button>
     </div>
   );
 }
---
+++ src/Testapp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Testapp = () => {
+  return (
+    <div><h1>testapp</h1></div>
+ 
+    )
+}
+
+
+export default Testapp
